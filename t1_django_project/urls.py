555eb96427604613f57910281aa2a8c3d0f7160e
gnacrur <42191903+gnacrur@users.noreply.github.com>
2020-04-01 01:47:14 -0300

    Add files via upload

---
+++ t1_django_project/urls.py
@@ -0,0 +1,27 @@
+"""t1_django_project URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from pages.views import home_view, capitulo_view, personaje_view, lugar_view, busqueda_view
+
+urlpatterns = [
+    path('', home_view, name='home'),
+    path('capitulo/<int:num>/', capitulo_view, name='capitulo'),
+    path('personaje/<int:num>/', personaje_view, name='personaje'),
+    path('lugar/<int:num>/', lugar_view, name='lugar'),
+    path('busqueda/', busqueda_view, name='busqueda'),
+    path('admin/', admin.site.urls),
+]
